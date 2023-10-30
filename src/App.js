3ce0e29f89e2826a2f25304bcb05d031ce2f9f85
bonganimasemola <bonganimasemola@icloud.com>
2023-10-30 17:39:50 +0300

    sort transactions by category or description edit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import fetchTransactions from './api';
 function App() {
   const [transactions, setTransactions] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [sortOrder, setSortOrder] = useState('asc'); // 'asc' for ascending, 'desc' for descending
+  const [sortBy, setSortBy] = useState('description'); // 'description' or 'category'
 
   useEffect(() => {
     fetchTransactions()
@@ -25,21 +27,61 @@ function App() {
     setTransactions(updatedTransactions);
   };
 
+  const sortTransactions = () => {
+    const sortedTransactions = [...transactions].sort((a, b) => {
+      const aValue = a[sortBy].toLowerCase();
+      const bValue = b[sortBy].toLowerCase();
+
+      if (aValue < bValue) {
+        return sortOrder === 'asc' ? -1 : 1;
+      }
+      if (aValue > bValue) {
+        return sortOrder === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+
+    setTransactions(sortedTransactions);
+  };
+
+  const toggleSortOrder = () => {
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+  };
+
+  const handleSortByChange = (event) => {
+    setSortBy(event.target.value);
+  };
+
   const filteredTransactions = transactions.filter((transaction) =>
     transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
     <div className="App">
-      <header className="App-header"><strong>BANK OF FLATIRON</strong></header>
+      <header className="App-header">Bank of Flatiron</header>
       <TransactionSubmitForm addTransaction={addTransaction} />
       <TransactionSearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      <div>
+        <label>
+          Sort by:
+          <select value={sortBy} onChange={handleSortByChange}>
+          <option value="description">Description</option>
+          <option value="category">Category</option>
+          </select>
+        </label>
+        <button onClick={sortTransactions}>
+          Sort {sortOrder === 'asc' ? 'Ascending' : 'Descending'}
+        </button>
+      </div>
+
       <TransactionTable transactions={filteredTransactions} onDeleteTransaction={deleteTransaction} />
     </div>
   );
 }
 
 export default App;
+
 
 
 
