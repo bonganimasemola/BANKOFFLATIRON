d181235672facdee61c691d9deb41ff684478876
bonganimasemola <bonganimasemola@icloud.com>
2023-10-30 11:57:23 +0300

    TransActionSubmitForm set up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import TransactionTable from './TransactionTable';
-import fetchTransactions  from './api'; 
- 
+import TransactionSubmitForm from './TransactionSubmitForm';
+import fetchTransactions from './api';
 
 function App() {
   const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
-    
     fetchTransactions()
       .then((data) => setTransactions(data))
       .catch((error) => console.error('Error fetching transactions:', error));
   }, []);
 
+  const addTransaction = (newTransaction) => {
+    
+    setTransactions([...transactions, newTransaction]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-      Code Challenge: Bank of Flatiron
-</header>
+      <header className="App-header">Bank of Flatiron</header>
+      <TransactionSubmitForm addTransaction={addTransaction} />
       <TransactionTable transactions={transactions} />
     </div>
   );
 }
 
 export default App;
+
 
---
+++ src/TransactionSubmitForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+function TransactionSubmitForm({ addTransaction }) {
+  const [formData, setFormData] = useState({
+    date: '',
+    description: '',
+    category: '',
+    amount: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTransaction(formData);
+    setFormData({
+      date: '',
+      description: '',
+      category: '',
+      amount: '',
+    });
+  };
+
+  return (
+    <div>
+      <h2>Add a New Transaction</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Date:
+          <input
+            type="text"
+            name="date"
+            value={formData.date}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Description:
+          <input
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Category:
+          <input
+            type="text"
+            name="category"
+            value={formData.category}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Amount:
+          <input
+            type="number"
+            name="amount"
+            value={formData.amount}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">Add Transaction</button>
+      </form>
+    </div>
+  );
+}
+
+export default TransactionSubmitForm;
